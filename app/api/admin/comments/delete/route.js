3547f4254aabb7f447cfacd76236f2e18c137806
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-02 00:40:33 +0500

    Delete app/api directory

--- app/api/admin/comments/delete/route.js
+++
@@ -1,9 +0,0 @@
-import { pool } from "@/services/db";
-
-export async function POST(req) {
-  const { id } = await req.json();
-
-  await pool.query("DELETE FROM comments WHERE id=$1", [id]);
-
-  return Response.json({ success: true });
-}
--- app/api/movies/list/route.js
+++
@@ -1,6 +0,0 @@
-import { pool } from "@/services/db";
-
-export async function GET() {
-  const data = await pool.query("SELECT * FROM movies ORDER BY id DESC");
-  return Response.json({ movies: data.rows });
-}
