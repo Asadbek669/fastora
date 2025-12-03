75983db5a5db43c140ec8773e880b212d69f270b
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-03 14:27:47 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,27 @@
-"use client"; 
 import "./globals.css";
 import TopBar from "./components/TopBar";
 import BottomNav from "./components/BottomNav";
 import { usePathname } from "next/navigation";
 
+export const metadata = {
+  title: "Fastora",
+  description: "Fastora — multfilm, anime, kino va drama platformasi",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/icons/icon-192.png"
+  }
+};
+
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-
-  // ADMIN sahifalardami? 
   const isAdmin = pathname.startsWith("/admin");
 
   return (
     <html lang="uz">
       <body>
-        {/* TopBar ham public ham admin sahifalarda bo'ladimi? 
-            Agar admin sahifalarda ham kerak bo'lmasa — xohlasa olib tashlaymiz */}
         <TopBar />
-
         {children}
-
-        {/* faqat public sahifalarda ko'rinadi */}
         {!isAdmin && <BottomNav />}
       </body>
     </html>
