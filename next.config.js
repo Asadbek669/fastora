ea19025820a365115e9161cc73de4a064f3cbcbc
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-04 22:39:52 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "pub-7b8b8366b149459fa661ebc5cb2caef0.r2.dev"
-    ]
+    remotePatterns: []
   }
 };
 
