8690d6f4cdadd2e8cbb9fbdf4f5ba68c5983d9f1
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-12 00:30:53 +0500

    Update convertEmbed.js

--- app/utils/convertEmbed.js
+++ app/utils/convertEmbed.js
@@ -1,13 +1,14 @@
+// utils/convertEmbed.js
 export default function convertToEmbed(url) {
-  if (url.includes("/shorts/")) {
-    const id = url.split("/shorts/")[1].split("?")[0];
-    return `https://www.youtube-nocookie.com/embed/${id}`;
+  if (!url) return '';
+  
+  // YouTube linklarini embed formatiga o'tkazish
+  const youtubeRegex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
+  const match = url.match(youtubeRegex);
+  
+  if (match && match[1]) {
+    return `https://www.youtube.com/embed/${match[1]}`;
   }
-
-  if (url.includes("watch?v=")) {
-    const id = url.split("watch?v=")[1].split("&")[0];
-    return `https://www.youtube-nocookie.com/embed/${id}`;
-  }
-
+  
   return url;
 }
