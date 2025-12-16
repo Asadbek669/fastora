05668a4874a68d52aa63f4c06429b8be95bb47f9
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-17 00:29:53 +0500

    Update page.jsx

--- app/tv/page.jsx
+++ app/tv/page.jsx
@@ -10,16 +10,14 @@ export default function TvPage() {
   return (
     <div className="pb-24">
 
-      {/* SAHIFA SARLAVHASI */}
       <h1 className="text-xl font-semibold px-4 pt-4 mb-4">
         📺 Telekanallar
       </h1>
 
-      {/* TELEKANALLAR GRID */}
       <div
         className="
-          grid gap-4 px-4
-          [grid-template-columns:repeat(auto-fit,minmax(110px,1fr))]
+          grid gap-3 px-4
+          [grid-template-columns:repeat(auto-fit,minmax(90px,1fr))]
         "
       >
         {tvChannels.map((tv, index) => (
@@ -27,21 +25,18 @@ export default function TvPage() {
             key={index}
             className="
               rounded-xl overflow-hidden
-              bg-[#111] shadow-lg
+              bg-[#111] shadow-md
               transition
               active:scale-95
-              hover:scale-[1.02]
             "
           >
-            {/* RASM */}
             <img
               src="/tv.jpg"
               alt={tv.name}
-              className="w-full h-28 object-cover"
+              className="w-full h-24 object-cover"
             />
 
-            {/* NOMI */}
-            <div className="p-2 text-center text-sm truncate">
+            <div className="py-1 text-center text-xs truncate">
               {tv.name}
             </div>
           </div>
