a112b00070cf9fc816419457e23c66fd3bc15ca3
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-17 00:21:37 +0500

    Create tvConfig.js

---
+++ app/tv/tvConfig.js
@@ -0,0 +1,32 @@
+const tvChannels = [
+  {
+    name: "O‘zbekiston 24",
+    slug: "uzbekiston-24",
+  },
+  {
+    name: "Milliy TV",
+    slug: "milliy-tv",
+  },
+  {
+    name: "Zo‘r TV",
+    slug: "zor-tv",
+  },
+  {
+    name: "MY5",
+    slug: "my5",
+  },
+  {
+    name: "Sport",
+    slug: "sport",
+  },
+  {
+    name: "Sevimli",
+    slug: "sevimli",
+  },
+  {
+    name: "Dunyo bo‘ylab",
+    slug: "dunyo-boylab",
+  },
+];
+
+export default tvChannels;
