51dcfe1a854393a214cc88bb4f1523048f22dee8
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-17 21:02:59 +0500

    Update tvConfig.js

--- app/tv/tvConfig.js
+++ app/tv/tvConfig.js
@@ -1,32 +1,41 @@
+const CDN_BASE = "https://cdn.fastora.uz/tv";
+
 const tvChannels = [
-  {
-    name: "O‘zbekiston 24",
-    slug: "uzbekiston-24",
-  },
-  {
-    name: "Milliy TV",
-    slug: "milliy-tv",
-  },
-  {
-    name: "Zo‘r TV",
-    slug: "zor-tv",
-  },
-  {
-    name: "MY5",
-    slug: "my5",
-  },
-  {
-    name: "Sport",
-    slug: "sport",
-  },
-  {
-    name: "Sevimli",
-    slug: "sevimli",
-  },
-  {
-    name: "Dunyo bo‘ylab",
-    slug: "dunyo-boylab",
-  },
-];
+  { name: "BIZ TV", slug: "biz-tv" },
+  { name: "Setanta 1", slug: "setanta-1" },
+  { name: "Россия 1", slug: "rossiya-1" },
+  { name: "Россия 24", slug: "rossiya-24" },
+  { name: "Первый канал", slug: "perviy-kanal" },
+  { name: "Muloqot TV", slug: "muloqot-tv" },
+  { name: "Istiqlol TV", slug: "istiqlol-tv" },
+  { name: "ZO‘R TV", slug: "zor-tv" },
+  { name: "Renessans TV", slug: "renessans-tv" },
+  { name: "Futbol TV", slug: "futbol-tv" },
+  { name: "Gold TV", slug: "gold-tv" },
+  { name: "NTV", slug: "ntv" },
+  { name: "Sevimli TV", slug: "sevimli-tv" },
+  { name: "MY5", slug: "my5" },
+  { name: "Milliy TV", slug: "milliy-tv" },
+  { name: "Dasturxon TV", slug: "dasturxon-tv" },
+  { name: "MYDAY TV", slug: "myday-tv" },
+  { name: "Madaniyat va Ma'rifat", slug: "madaniyat-marifat" },
+  { name: "Toshkent TV", slug: "toshkent-tv" },
+  { name: "Dunyo Bo‘ylab", slug: "dunyo-boylab" },
+  { name: "FTV", slug: "ftv" },
+  { name: "Bolajon TV", slug: "bolajon-tv" },
+  { name: "Navo", slug: "navo" },
+  { name: "Mahalla TV", slug: "mahalla-tv" },
+  { name: "SPORT TV UZ", slug: "sport-tv-uz" },
+  { name: "O'zbekiston tarixi", slug: "ozbekiston-tarixi" },
+  { name: "Kinoteatr", slug: "kinoteatr" },
+  { name: "UZ REPORT TV", slug: "uzreport-tv" },
+  { name: "O‘zbekiston 24", slug: "uzbekiston-24" },
+  { name: "O‘zbekiston 1", slug: "uzbekiston-1" },
+  { name: "Yoshlar", slug: "yoshlar" },
+].map((tv) => ({
+  ...tv,
+  image: `${CDN_BASE}/${tv.slug}.webp`,
+  live: true,
+}));
 
 export default tvChannels;
