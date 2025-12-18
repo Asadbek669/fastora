f280a7235dc5db6ca2fd54e687d9c4ad0b20fe96
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-18 17:46:45 +0500

    Update page.jsx

--- app/tv/page.jsx
+++ app/tv/page.jsx
@@ -2,9 +2,37 @@ import Link from "next/link";
 import tvChannels from "./tvConfig";
 
 export const metadata = {
-  title: "Telekanallar | Fastora",
-  description: "Fastora platformasidagi barcha jonli telekanallar",
+  title: "Bepul Telekanallar — Fastora TV | Fastora",
+  description:
+    "Fastora orqali O‘zbekiston va xorijiy jonli telekanallarni onlayn tomosha qiling: Sevimli TV, MY5, Milliy TV, ZO‘R TV, O‘zbekiston 24 va boshqalar.",
+
+  alternates: {
+    canonical: "https://fastora.uz/tv",
+  },
+
+  openGraph: {
+    title: "Bepul telekanallar — Fastora",
+    description:
+      "Barcha mashhur jonli telekanallarni Fastora platformasida bepul tomosha qiling.",
+    url: "https://fastora.uz/tv",
+    siteName: "Fastora",
+    locale: "uz_UZ",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary",
+    title: "Jonli telekanallar — Fastora",
+    description:
+      "Bepul jonli efirdagi TV kanallar Fastora’da",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
+
 
 export default function TvPage() {
   return (
