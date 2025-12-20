44bab01e5fa50859afc25fc6699e0d863a95abd9
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-20 23:05:33 +0500

    Update remote image hostname in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**"
-      }
-    ]
-  }
+        hostname: "cdn.fastora.uz",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
