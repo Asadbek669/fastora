da502a874e333c264a87f958c091f368ee363c6e
Asadbek669 <akbarovasadbek777@gmail.com>
2025-12-21 00:10:44 +0500

    Add revalidation for GET stories API
    Added revalidation for caching with a 3000ms duration.

--- app/api/stories/route.js
+++ app/api/stories/route.js
@@ -1,6 +1,11 @@
 import { pool } from "@/services/db";
 
+export const revalidate = 3000; // 🔥 50 daqiqa cache
+
 export async function GET() {
-  const { rows } = await pool.query("SELECT * FROM stories ORDER BY id DESC");
+  const { rows } = await pool.query(
+    "SELECT * FROM stories ORDER BY id DESC"
+  );
+
   return Response.json(rows);
 }
