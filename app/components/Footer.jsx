2bbc718e17b9aaa5b2b3ee3eb88fb2d889c1f505
Asadbek669 <akbarovasadbek777@gmail.com>
2026-01-01 16:29:54 +0500

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,15 +2,27 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mt-12 py-6 text-center">
-      <p className="text-xs text-gray-500">
+    <footer className="mt-2 py-1 text-center">
+      <p className="text-xs text-gray-500 leading-tight">
         © 2025 Fastora TV Platformasi —{" "}
         <Link
           href="/copyright"
-          className="text-blue-400 hover:text-blue-300 transition"
+          className="text-blue-400 hover:text-blue-300"
         >
           Mualliflik huquqi
         </Link>
+      </p>
+
+      <p className="text-xs text-gray-500 leading-tight mt-[2px]">
+        Biz bilan aloqa:{" "}
+        <a
+          href="https://t.me/fastora_admin"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 hover:text-blue-300"
+        >
+          TELEGRAM
+        </a>
       </p>
     </footer>
   );
