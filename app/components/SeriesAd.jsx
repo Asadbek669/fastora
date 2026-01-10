4a7ee13922129cc3c2ef291a4a6e6555d8d2c703
Asadbek669 <akbarovasadbek777@gmail.com>
2026-01-11 00:32:58 +0500

    Create SeriesAd.jsx

---
+++ app/components/SeriesAd.jsx
@@ -0,0 +1,13 @@
+"use client";
+import Script from "next/script";
+
+export default function SeriesAd() {
+  return (
+    <Script
+      src="https://quge5.com/88/tag.min.js"
+      data-zone="200783"
+      strategy="afterInteractive" // sahifa yuklangach ishlaydi
+      data-cfasync="false"
+    />
+  );
+}
