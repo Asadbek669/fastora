deb20acb83f72c571df2d5a657bba9260982b7b7
Asadbek669 <akbarovasadbek777@gmail.com>
2026-01-13 16:11:10 +0500

    Update page.jsx

--- app/serial/[slug]/season/[season]/page.jsx
+++ app/serial/[slug]/season/[season]/page.jsx
@@ -2,7 +2,6 @@ import { cache } from "react";
 import SeriesDetail from "@/components/SeriesDetail";
 import SeasonList from "@/components/SeasonList";
 import EpisodeList from "@/components/EpisodeList";
-import SeriesAd from "@/components/SeriesAd";
 
 const base = process.env.NEXT_PUBLIC_SITE_URL;
 
@@ -101,11 +100,7 @@ export default async function SeasonPage({ params }) {
       {/* ⭐ Sahifa UI ⭐ */}
       <div className="bg-black text-white pb-24">
         <SeriesDetail series={series} />
-        
-        <div className="my-6">
-          <SeriesAd />
-        </div>
-        
+
         <div className="px-4 mt-6">
           <SeasonList slug={slug} seasons={seasons} activeSeason={season} />
           <EpisodeList slug={slug} season={season} episodes={episodes} />
