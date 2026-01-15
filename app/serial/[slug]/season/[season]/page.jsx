6d47195519056d7d636f14fd52cc36578f7ac2ff
Asadbek669 <akbarovasadbek777@gmail.com>
2026-01-15 17:04:43 +0500

    Update page.jsx

--- app/serial/[slug]/season/[season]/page.jsx
+++ app/serial/[slug]/season/[season]/page.jsx
@@ -28,7 +28,7 @@ const getSeasons = cache(async (slug) => {
 const getEpisodesBySeason = cache(async (id) => {
   const res = await fetch(`${base}/api/episode/season/${id}`, {
     next: {
-      revalidate: 1, // ✅ 0.5 kun
+      revalidate: 86400, // ✅ 0.5 kun
     },
   });
   return res.json();
